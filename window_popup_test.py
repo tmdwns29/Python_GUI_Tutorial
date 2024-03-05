4ae53d45ae930a77c28336abe487c8d7a1a1b52a
YooSeungJun <12000ha@naver.com>
2024-03-05 23:37:51 +0900

    주석 수정

--- window_popup_test.py
+++ window_popup_test.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 # 기본적인 UI 구성요소를 제공하는 위젯은 PyQt5.QtWidgets모듈에 포함됨
 
+# 기본적인 윈도우 창 생성
 class MyApp(QWidget): # MyApp 객체 선언, QWidget클래스 상속받음
     def __init__(self): # self는 MyApp 객체를 말함
         super().__init__() # 부모 클래스의 생성자 호출
@@ -14,7 +15,7 @@ class MyApp(QWidget): # MyApp 객체 선언, QWidget클래스 상속받음
         self.show() # 위젯을 스크린에 보여줌
 
 if __name__ == '__main__': # __name__은 현재 모듈의 이름이 저장되는 내장 변수
-    app = QApplication(sys.argv) # 겍체 생성
-    ex = MyApp()
+    app = QApplication(sys.argv) # 객체 생성
+    ex = MyApp() # MyApp() 객체 생성 -> QWidget 상속받음
     sys.exit(app.exec_()) # 애플리케이션이 정상적으로 종료하기 위해 호출
     # app.exec_()호출은 프로그램이 종료될 때까지 계속 실행
